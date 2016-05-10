9b539d8b56619dac2e673567739cc0749cf74290
Elio Struyf <info@estruyf.be>
2016-05-10 16:55:43 +0200

    Initial commit

---
+++ estruyf.MailComposeAddinWeb/ComposeForm/Form.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    // The Office initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Write some code
+        });
+    };
+})();
